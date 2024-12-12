d2d64662c4b7c5fea9a1e46d653af14a4c3015d6
WorldWideTurtle <148323035+WorldWideTurtle@users.noreply.github.com>
2024-12-12 19:26:37 +0100

    Add Invite link page

---
+++ app/invites/[uuid]/page.tsx
@@ -0,0 +1,35 @@
+import { createClient } from "@/utils/supabase/server";
+import { PostgrestError } from "@supabase/supabase-js";
+import { redirect } from "next/navigation";
+
+export default async function Page({params} : {params : Promise<{uuid:string}>}) {
+    const [supabase, {uuid}] = await Promise.all([
+        createClient(),
+        params
+    ]);
+
+    async function DeleteOldEntries() {
+        const { data, error } : {data : {invite_id:string}[] | null, error : PostgrestError |null} = await supabase.rpc("delete_old_invite_links")
+        
+
+        if (data && data.find(e=>e.invite_id === uuid)) {return <h1>This invite either doesn't exist, or has expired</h1>};
+    }
+    
+    await DeleteOldEntries()
+
+    const [{data : { user }},{error}, {data}] = await Promise.all([
+        supabase.auth.getUser(),
+        supabase.rpc("add_user_to_project",{invite_uuid:uuid}),
+        supabase.from("invite_links").select().eq("uuid",uuid).single()
+    ])
+
+    if (!data) {
+        return <h1>This invite either doesn't exist, or has expired</h1>
+    }
+
+    if (!user) {
+        return redirect("/sign-in?invite=" + uuid);
+    }
+
+    return redirect("/worlds")
+}
