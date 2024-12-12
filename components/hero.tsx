bd5e00190115950ee207b40c2425e9e3cb2876dc
WorldWideTurtle <148323035+WorldWideTurtle@users.noreply.github.com>
2024-12-12 20:55:56 +0100

    Add temp text to hero

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,7 +1,10 @@
 export default function Header() {
   return (
     <div className="flex flex-col gap-16 items-center">
-      Hero
+      <div className="mt-8 text-center">
+        <h2 className="text-4xl">The Enderchest</h2>
+        <h2 className="text-xl -mt-2">For your Enderchests</h2>
+      </div>
     </div>
   );
 }
