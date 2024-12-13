970bc19066f3462157ee18261face346e36a05db
WorldWideTurtle <148323035+WorldWideTurtle@users.noreply.github.com>
2024-12-13 13:55:06 +0100

    Add Middleware clause to protect worlds

--- middleware.ts
+++ middleware.ts
@@ -4,13 +4,19 @@ import { createClient } from "./utils/supabase/server";
 
 export async function middleware(request: NextRequest) {
   const urlPaths = request.nextUrl.pathname.split("/")
-  if (urlPaths[1] === "worlds" && urlPaths.length > 3) {
-      const supabase = await createClient()
-      const {data} = await supabase.rpc("get_user_role_in_project",{p_uuid:urlPaths[2]})
-      if (data !== 1) {
-        request.nextUrl.pathname = "/worlds/" + urlPaths[2]
-        return NextResponse.redirect(request.nextUrl)
-      }
+  if (urlPaths[1] === "worlds") {
+    const supabase = await createClient()
+    const {data} = await supabase.rpc("get_user_role_in_project",{p_uuid:urlPaths[2]})
+
+    if (urlPaths.length > 2 && data === null) {
+      request.nextUrl.pathname = "/worlds"
+      return NextResponse.redirect(request.nextUrl)
+    }
+
+    if (urlPaths.length > 3 && data !== 1) {
+      request.nextUrl.pathname = "/worlds/" + urlPaths[2]
+      return NextResponse.redirect(request.nextUrl)
+    }
   }
   return await updateSession(request);
 }
