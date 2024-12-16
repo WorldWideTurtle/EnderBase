2f70aaf01ee470a37cdfc1ed9e1b194fb7c4e50c
WorldWideTurtle <148323035+WorldWideTurtle@users.noreply.github.com>
2024-12-16 19:44:23 +0100

    Fix Logo overlap on firefox

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,7 +40,7 @@ export default async function RootLayout({
               <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
                 <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
                   <div className="flex gap-5 items-center font-semibold">
-                    <Link href={"/"} className="font-bold md:text-xl grid grid-cols-[auto_auto] items-center h-full gap-1"> <LogoIcon className="h-full w-auto"/> <span className="py-2 hidden md:inline">EnderBase</span></Link>
+                    <Link href={"/"} className="font-bold md:text-xl grid grid-cols-[auto_auto] items-center h-full gap-1"> <LogoIcon className="h-full"/> <span className="py-2 hidden md:inline">EnderBase</span></Link>
                   </div>
                   <div className="flex items-center gap-1">
                     <ThemeSwitcher />
