9e4b4843387cfcb70ad37a331e1d0a750cb7b014
WorldWideTurtle <148323035+WorldWideTurtle@users.noreply.github.com>
2024-12-23 00:55:45 +0100

    Emulate end-portal for BG, add solid BG to header and footer

--- components/hero-bg.tsx
+++ components/hero-bg.tsx
@@ -1,76 +1,52 @@
 'use client'
 
-import { useEffect, useState } from "react"
-
-function GetBase() {
-    return (new Array(300)).fill(0).map(e=>{
-        return [
-            RoundTo(Math.random()*10 + 5,1),
-            RoundTo(Math.random()*20 + 5,1),
-            RoundTo(Math.random()*100,1),
-            RoundTo(Math.random()*100,1),
-            RoundTo(0.8 - Math.random() * 1,1)
-        ]
-    })
-}
-
-function RoundTo(x: number, digits: number) {
-    return Math.round(x * Math.pow(10,digits)) / (Math.pow(10,digits))
-}
-
-function Clamp(x: number, min: number, max:number) {
-    return Math.min(max,Math.max(x,min));
-}
+import { createRef, RefObject, useEffect } from "react";
+import classes from "./bg.module.css"
 
 export function HeroBG() {
-    const [points,setPoints] = useState<number[][]>(GetBase())
+    const layerCount = 10;
+    const elementRefs : RefObject<null | HTMLDivElement>[] = (new Array(layerCount)).fill(0).map(e=>createRef())
 
     useEffect(()=>{
-        let nextFrame : number = 0
+        const canvas = document.createElement("canvas");
+        const ctx = canvas.getContext("2d");
 
-        const getNewPoints = () => {
-            const newPoints = [...points].map(e=>{
-                return [
-                    e[0],
-                    e[1],
-                    Clamp(RoundTo(e[2] + Math.random()*5,1),0,window.innerWidth),
-                    Clamp(RoundTo(e[3] + Math.random()*5,1),0,window.innerHeight),
-                    e[4]
-                ]
+        if (ctx === null) return;
+
+        canvas.width = 128
+        canvas.height = 128;
+
+        const img = new Image();
+        img.crossOrigin = "Anonymous"; // Allows canvas operations on cross-origin images
+        img.src = "/EP.webp";
+
+        img.onload = () => {
+            elementRefs.forEach(e=>{
+                if (!e.current) return;
+                ctx.clearRect(0,0,128,128)
+                ctx.filter = `hue-rotate(${Math.round(180 + Math.random() * 60)}deg)`;
+                ctx.drawImage(img, 0, 0);
+                let dataURL = canvas.toDataURL()
+    
+                const startX = Math.round(50 - Math.random() * 100);
+                const startY = Math.round(50 - Math.random() * 100);
+                const scale = Math.round(256 + Math.random() * 512*4);
+                const elementStyle = e.current.style
+                elementStyle.backgroundImage = `url("${dataURL}")`
+                elementStyle.setProperty("--start-x", startX + "px")
+                elementStyle.setProperty("--start-y", startY + "px")
+                elementStyle.setProperty("--end-x", (startX + scale * ((Math.random() > 0.5) ? 1 : -1)) + "px")
+                elementStyle.setProperty("--end-y", (startY + scale * ((Math.random() > 0.5) ? 1 : -1)) + "px")
+                elementStyle.animationDuration = 1000 * (scale / 2560)**1.5 + "s"
+                elementStyle.backgroundSize = `${scale}px ${scale}px`
             })
-            setPoints(newPoints);
         }
-        
-        let interval : NodeJS.Timeout | null = null;
-        const timeOut = setTimeout(()=>{
-            nextFrame = requestAnimationFrame(getNewPoints)  
-            interval = setInterval(()=>{
-                nextFrame = requestAnimationFrame(getNewPoints)  
-            },3000)
-        }, 250)
-
-        return () => {
-            if (interval) clearInterval(interval)
-            clearTimeout(timeOut)
-            cancelAnimationFrame(nextFrame)
-        }
-    },[])
+    })
 
     return (
-        <div className="absolute w-full h-full top-0 -z-20 isolate overflow-hidden">
-            {points.map((e,i)=>(
-                <div key={i} suppressHydrationWarning style={{
-                    width: e[0],
-                    height: e[1],
-                    transform: `translateX(${e[2] * window.innerWidth / 100}px) translateY(${e[3] * window.innerHeight / 100}px)`,
-                    position: "absolute",
-                    left: 0,
-                    top: 0,
-                    background: "#1e403f",
-                    filter: "blur(3px)",
-                    transition: "transform 4s ease",
-                    opacity: e[4]
-                }}></div>
+        <div className="absolute w-full h-full top-0 -z-20 isolate overflow-hidden opacity-30 contain-strict">
+            {(new Array(layerCount)).fill(0).map((e,i)=>(
+                <div key={i} ref={elementRefs[i]} className={"w-full h-full bg-transparent absolute left-0 top-0 " + classes.animate}></div>
             ))}
         </div>
     )
