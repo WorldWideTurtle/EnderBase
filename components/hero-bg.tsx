ae6d014027569baa81eb67895f19090a6d60a7ef
WorldWideTurtle <148323035+WorldWideTurtle@users.noreply.github.com>
2024-12-23 01:22:46 +0100

    Add fallback BG if canvas is not supported

--- components/hero-bg.tsx
+++ components/hero-bg.tsx
@@ -1,27 +1,37 @@
 'use client'
 
-import { createRef, RefObject, useEffect } from "react";
+import { createRef, RefObject, useEffect, useRef } from "react";
 import classes from "./bg.module.css"
 
 export function HeroBG() {
-    const layerCount = 10;
-    const elementRefs : RefObject<null | HTMLDivElement>[] = (new Array(layerCount)).fill(0).map(e=>createRef())
+    const src = "/end_portal.webp";
+    const layerCount = 5;
+    const elementRefs = (new Array(layerCount)).fill(0).map(e=>createRef<HTMLDivElement>())
+    const fallbackRef = useRef<HTMLDivElement>(null)
 
     useEffect(()=>{
         const canvas = document.createElement("canvas");
         const ctx = canvas.getContext("2d");
 
-        if (ctx === null) return;
-
+        if (ctx === null) {
+            if (fallbackRef.current)
+                fallbackRef.current.style.display = "block";
+                elementRefs.forEach(e=>{
+                    if (e.current) e.current.remove()
+                })
+            return;
+        };
+        
         canvas.width = 256
         canvas.height = 256;
 
         const img = new Image();
         img.crossOrigin = "Anonymous"; // Allows canvas operations on cross-origin images
-        img.src = "/end_portal.webp";
+        img.src = src;
 
         img.onload = () => {
             elementRefs.forEach(e=>{
+                if (fallbackRef.current) fallbackRef.current.remove();
                 if (!e.current) return;
                 ctx.clearRect(0,0,256,256)
                 ctx.filter = `hue-rotate(${Math.round(180 + Math.random() * 60)}deg)`;
@@ -45,6 +55,29 @@ export function HeroBG() {
 
     return (
         <div className="absolute w-full h-full top-0 -z-20 isolate overflow-hidden opacity-30 contain-strict">
+            <div ref={fallbackRef} className="hidden">
+                <div className="w-full h-full bg-transparent absolute left-0 top-0" style={{
+                    backgroundImage: `url("${src}")`,
+                    backgroundSize: "2048px 2048px",
+                    backgroundPosition: "-1200px 400px",
+                    imageRendering: "pixelated",
+                    filter: "hue-rotate(200deg)"
+                }}></div>
+                <div className="w-full h-full bg-transparent absolute left-0 top-0" style={{
+                    backgroundImage: `url("${src}")`,
+                    backgroundSize: "1024px 1024px",
+                    backgroundPosition: "-300px 800px",
+                    imageRendering: "pixelated",
+                    filter: "hue-rotate(200deg)"
+                }}></div>
+                <div className="w-full h-full bg-transparent absolute left-0 top-0" style={{
+                    backgroundImage: `url("${src}")`,
+                    backgroundSize: "1536px 1536px",
+                    backgroundPosition: "500px 400px",
+                    imageRendering: "pixelated",
+                    filter: "hue-rotate(200deg)"
+                }}></div>
+            </div>
             {(new Array(layerCount)).fill(0).map((e,i)=>(
                 <div key={i} ref={elementRefs[i]} className={"w-full h-full bg-transparent absolute left-0 top-0 " + classes.animate}></div>
             ))}
