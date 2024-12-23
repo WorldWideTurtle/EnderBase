3cb9de2fe583740329a560abd147a18325625354
WorldWideTurtle <148323035+WorldWideTurtle@users.noreply.github.com>
2024-12-23 01:40:06 +0100

    Fix hero bg only going along diagonals

--- components/hero-bg.tsx
+++ components/hero-bg.tsx
@@ -38,15 +38,18 @@ export function HeroBG() {
                 ctx.drawImage(img, 0, 0);
                 let dataURL = canvas.toDataURL()
     
+                const scale = Math.round(1024 + Math.random() * 512*4);
                 const startX = Math.round(50 - Math.random() * 100);
                 const startY = Math.round(50 - Math.random() * 100);
-                const scale = Math.round(1024 + Math.random() * 512*4);
+                const degreeOfRotation = Math.random() * Math.PI * 2;
+                const endX = startX + Math.sin(degreeOfRotation ) * scale;
+                const endY = startY + Math.cos(degreeOfRotation ) * scale;
                 const elementStyle = e.current.style
                 elementStyle.backgroundImage = `url("${dataURL}")`
                 elementStyle.setProperty("--start-x", startX + "px")
                 elementStyle.setProperty("--start-y", startY + "px")
-                elementStyle.setProperty("--end-x", (startX + scale * ((Math.random() > 0.5) ? 1 : -1)) + "px")
-                elementStyle.setProperty("--end-y", (startY + scale * ((Math.random() > 0.5) ? 1 : -1)) + "px")
+                elementStyle.setProperty("--end-x", endX + "px")
+                elementStyle.setProperty("--end-y", endY + "px")
                 elementStyle.animationDuration = 1000 * (scale / 2560)**1.5 + "s"
                 elementStyle.backgroundSize = `${scale}px ${scale}px`
             })
